b138b83b073254a9224dd7adb7fcc7684dc5f6ac
PatriotRossii <patriotrossii2019@mail.ru>
2023-06-26 14:17:37 +0300

    Cook breakfast asynchronously

--- src/main.cpp
+++ src/main.cpp
@@ -19,18 +19,35 @@ int main() {
 	net::io_context io;
 	const auto start_time = steady_clock::now();
 	{
-		net::steady_timer t(io, 5s);
+		auto t = std::make_shared<net::steady_timer>(io, 5s);
 		std::cout << "Brew coffee"sv << std::endl;
-		t.wait();
-		std::cout << "Pour coffee in the cup"sv << std::endl;
+		t->async_wait([t](sys::error_code ec) {
+			if (ec) {
+				throw std::runtime_error("Wait error: "s + ec.message());
+			}
+			std::cout << "Pour coffee in the cup. Thread id: "sv << std::this_thread::get_id()
+					  << std::endl;
+		});
 	}
 	{
 		std::cout << "Fry eggs"sv << std::endl;
-		net::steady_timer t(io, 3s);
-		t.wait();
-		std::cout << "Put eggs onto the plate" << std::endl;
+		auto t = std::make_shared<net::steady_timer>(io, 3s);
+		t->async_wait([t](sys::error_code ec) {
+			if (ec) {
+				throw std::runtime_error("Wait error: "s + ec.message());
+			}
+			std::cout << "Put eggs onto the plate. Thread id: "sv << std::this_thread::get_id()
+					  << std::endl;
+		});
 	}
-	const auto cook_duration = duration<double>(steady_clock::now() - start_time);
-	std::cout << "Breakfast has been cooked in "sv << cook_duration.count() << "s"sv << std::endl;
-	std::cout << "Enjoy your meal"sv << std::endl;
+	try {
+	    std::cout << "Run asynchronous operations"sv << std::endl;
+	    io.run();
+		const auto cook_duration = duration<double>(steady_clock::now() - start_time);
+		std::cout << "Breakfast has been cooked in "sv << cook_duration.count() << "s"sv << std::endl;
+        std::cout << "Thread id: "sv << std::this_thread::get_id() << std::endl;
+		std::cout << "Enjoy your meal"sv << std::endl;
+	} catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
 }
